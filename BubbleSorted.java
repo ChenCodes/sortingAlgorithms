00dee8553218a14f07101efda7416151fb39a900
Jack Chen <yuxi.chen@berkeley.edu>
2016-06-29 21:30:36 -0700

    Update BubbleSorted.java

--- BubbleSorted.java
+++ BubbleSorted.java
@@ -1,38 +1,25 @@
 import java.util.*;
 
 public class BubbleSorted {
-	
 	public static void bubble_sort(int array[]) {
-
-
-
 		//Keep around a variable n that will be assigned to the array's length since we want to iterate in the outer loop a total of the array's length times
 		int n = array.length;
-
-
 		int k;
 		for (int m = n; m >= 0; m--) {
-
 			//The inner for loop helps us keep track of a second pointer to the next element in the array. So, we'd be able to look at index 0 and index 1 and compare them, then index 1 and index 2. 
 			for (int i = 0; i < n - 1; i++) {
-
 				//This line lets us look at the next upcoming element (k is right after i).
 				k = i + 1;
-
 				//if the second element is greater than the first element, then we want to swap. 
 				if (array[k] < array[i]) {
-
 					//call the method below, passing in the array as well (we're doing destructive modification).
 					swapNumbers(i, k, array);
 				}
 			}
-			
 		}
-		
 	}
 
 	private static void swapNumbers(int i, int j, int[] array)  {
-
 		//first we want to store array[i] into a temporary variable so that we can store the var into array[j]
 		int temp;
 		temp = array[i];
